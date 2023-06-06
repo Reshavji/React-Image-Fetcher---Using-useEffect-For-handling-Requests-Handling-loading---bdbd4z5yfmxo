b25609856afcb23e308efb4c446b2ef3a6b38c8d
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-06-06 11:41:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import '../styles/App.css';
 import { Loader } from './Loader';
 import { PhotoFrame } from './PhotoFrame';
-
 const App = () => {
-const [id, setId] = useState('');
-const [loading, setLoading] = useState(false);
-const [photoData, setPhotoData] = useState(null);
+    const [load, setLoad] = useState(false);
+    const [url, setUrl] = useState('');
+    const [title, setTitle] = useState('');
+    const [flag, setFlag] = useState(false);
+    const handleChagne = async(e) => {
+        let id = e.target.value;
+        if (id >=1 && id <= 5000) {
+        setLoad(true);
+        setFlag(false)
+      
+        const response = await fetch(`https://jsonplaceholder.typicode.com/photos/${id}`);
 
-const fetchData = async () => {
-setLoading(true);
-try {
-const response = await fetch(https://jsonplaceholder.typicode.com/photos/${id});
-const data = await response.json();
-setPhotoData(data);
-setLoading(false);
-} catch (error) {
-console.log(error);
-setLoading(false);
-}
-};
+        const data = await response.json();
+        setLoad(false);
+        setTitle(data.title);
+        setUrl(data.url)
+        setFlag(true)
+       
+        console.log(data);
+        }
 
-const handleInputChange = (event) => {
-setId(event.target.value);
-};
+    }
 
-const handleSubmit = (event) => {
-event.preventDefault();
-fetchData();
-};
-
-return (
-<div>
-<form onSubmit={handleSubmit}>
-<input type="number" value={id} onChange={handleInputChange} placeholder="Enter a number" />
-<button type="submit">Submit</button>
-</form>
-
-php
-Copy code
-  {loading ? (
-    <Loader />
-  ) : (
-    photoData && (
-      <PhotoFrame url={photoData.url} title={photoData.title} />
+    return (
+        <>
+            <label>Id number</label>
+            <input type='number' onChange={handleChagne}/>
+            {load && <Loader/>}
+            {flag && <PhotoFrame url={url} title={title}/>}
+        </>
     )
-  )}
-</div>
-);
-};
+}
 
-export default App;
+export default App;
